4f921864018b92e50cfe95d0f9e651db0f4fe561
Miri Michaelson <miriammichaelson@gmail.com>
2020-07-21 15:24:59 +0100

    green test - test note starts with empty array of notes

--- spec/note-tests.js
+++ spec/note-tests.js
@@ -1,21 +1,23 @@
 'use strict';
 
 (function(exports) {
+  let noteApp = new NoteApp();
+
     function testNoteTitleIsStored() {
-      var noteApp = new NoteApp(); //This is creating the note
+      // var noteApp = new NoteApp(); //This is creating the note
       noteApp.note("test note")
       // debugger;
       expect.toEqual("test note title is stored", noteApp.noteArray.includes("test note"), true); //This is the expect line
     };
 
-  testNoteTitleIsStored(); //This is running the test
+      function testNoteArrayStartsEmpty() {
+        // var noteApp = new NoteApp();
+        expect.toEqual("test note starts with an empty array of notes", noteApp.noteArray.length, 0);
+      }
+
+      testNoteArrayStartsEmpty();
+      testNoteTitleIsStored(); //This is running the test
   })(this); //This gives us back the responses
 
-  //
   // (function(exports) {
-  //   function testNoteArrayStartsEmpty() {
-  //     expect.toEqual("test note starts with an empty array of notes", Note.noteArray.length, 0);
-  //   }
-  //
-  //   testNoteArrayStartsEmpty();
   // })(this);
